2fea88eca178696b24cb8676a33c0ef6a3c01af4
h2owater425 <h2o@h2owr.xyz>
2021-08-01 01:33:33 +0900

    chore: Build

--- library/index.d.ts
+++ library/index.d.ts
@@ -1,38 +1,46 @@
 export as namespace hitomi;
 
-export interface Tag {
-	type: 'artist' | 'group' | 'type' | 'language' | 'series' | 'character' | 'male' | 'female' | 'tag';
-	name: string;
-	isNegative?: boolean;
+export declare interface Image {
+    index: number;
+    hash: string;
+    extension: 'jpg' | 'png';
+    hasAvif: boolean;
+    hasWebp: boolean;
+    width: number;
+    height: number;
 }
 
-export interface Image {
-	index: number;
-	hash: string;
-	extension: 'jpg' | 'png';
-	hasAvif: boolean;
-	hasWebp: boolean;
-	width: number;
-	height: number;
+export declare type TagType = 'artist' | 'group' | 'type' | 'language' | 'series' | 'character' | 'male' | 'female' | 'tag';
+
+export declare interface Tag {
+    type: TagType;
+    name: string;
+    isNegative?: boolean;
 }
 
-export interface Gallery {
-	id: number;
-	title: string;
-	titleJapanese: string | null;
-	type: 'doujinshi' | 'manga' | 'artistcg' | 'gamecg';
-	language: string | null;
-	languageLocalName: string | null;
-	artists: string[];
-	groups: string[];
-	series: string[];
-	characters: string[];
-	tags: Tag[];
-	files: Image[];
-	publishedDate: Date;
+export declare interface Gallery {
+    id: number;
+    title: {
+        display: string;
+        japanese: string | null;
+    };
+    type: 'doujinshi' | 'manga' | 'artistcg' | 'gamecg' | 'anime';
+    languageName: {
+        english: string | null;
+        local: string | null;
+    };
+    artistList: string[];
+    groupList: string[];
+    seriesList: string[];
+    characterList: string[];
+    tagList: Tag[];
+    fileList: Image[];
+    publishedDate: Date;
 }
 
-export type OrderCriteria = 'index' | 'popularity';
+export declare type OrderCriteria = 'index' | 'popularity';
+
+export declare type StartingCharacter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | '123';
 
 /**
  * Returns gallery data from hitomi from id
@@ -48,13 +56,13 @@ export declare function getGalleryData(id: number, option?: { includeFullData?: 
  * Returns gallery id list from hitomi from range
  * @param  {Object} range
  * @param  {number} range.startIndex
- * @param  {number} [range.endIndex=range.startIndex+1]
+ * @param  {number} [range.endIndex] If not set, the funtion will return list cotains every id
  * @param  {Object} [option]
- * @param  {OrderCriteria} [option.orderCriteria='index'] If set to 'popularity', the function will return id list order by popularity
- * @param  {boolean} [option.reverse=false] If set to true, the function will return reversed id list
+ * @param  {OrderCriteria} [option.orderBy='index'] If set to 'popularity', the function will return id list order by popularity
+ * @param  {boolean} [option.reverseResult=false] If set to true, the function will return reversed id list
  * @returns {Promise<number[]>} Promise
  */
-export declare function getGalleryIdList(range: { startIndex: number; endIndex?: number; }, option?: { orderCriteria?: OrderCriteria, reverse?: boolean; }): Promise<number[]>;
+export declare function getGalleryIdList(range: { startIndex: number; endIndex?: number; }, option?: { orderBy?: OrderCriteria; reverseResult?: boolean; }): Promise<number[]>;
 
 /**
  * Returns tag list from string
@@ -69,6 +77,14 @@ export declare function parseTag(tagString: string): Tag[];
  * @returns {Promise<number[]>} Promise
  */
 export declare function queryTag(tagList: Tag[]): Promise<number[]>;
+
+/**
+ * Returns tag list starting with specific character
+ * @param  {TagType} type
+ * @param  {StartingCharacter} [startingCharacter] If set and type isn't language, the function will return list of tag that starts with that character
+ * @returns Promise
+ */
+export declare function getTagList(type: TagType, startingCharacter?: StartingCharacter): Promise<Tag[]>;
 
 /**
  * Returns image url from image 
@@ -91,17 +107,27 @@ export declare function getGalleryUrl(galleryData: Gallery): string;
  * Returns nozomi url from tag
  * @param  {Tag} tag
  * @param  {Object} [option]
- * @param  {OrderCriteria} [option.orderCriteria='index'] If set to 'popularity', the function will return nozomi url that responses data order by popularity
+ * @param  {OrderCriteria} [option.orderBy='index'] If set to 'popularity', the function will return nozomi url that responses data order by popularity
  * @returns {string} string
  */
-export declare function getNozomiUrl(tag: Tag, option?: { orderCriteria?: OrderCriteria }): string;
+export declare function getNozomiUrl(tag: Tag, option?: { orderBy?: OrderCriteria; }): string;
+
+/**
+ * Returns tag list url with specific character
+ * @param  {TagType} type
+ * @param  {StartingCharacter} [startingCharacter] If set and type isn't language, the function will return hitomi url that responses data that starts with that character
+ * @returns string
+ */
+export declare function getTagUrl(type: TagType, startingCharacter?: StartingCharacter): string;
 
 export default {
-	getGalleryData,
-	getGalleryIdList,
-	parseTag,
-	queryTag,
-	getImageUrl,
-	getGalleryUrl,
-	getNozomiUrl
+	getGalleryData: getGalleryData,
+	getGalleryIdList: getGalleryIdList,
+	parseTag: parseTag,
+	queryTag: queryTag,
+	getTagList: getTagList,
+	getImageUrl: getImageUrl,
+	getGalleryUrl: getGalleryUrl,
+	getNozomiUrl: getNozomiUrl,
+	getTagUrl: getTagUrl
 };
