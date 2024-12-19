a5df3a34252c857bccb41f58ffe7340b8adc75f1
H2Owater425 <h2o@dhmo.kr>
2024-12-19 23:34:34 +0900

    fix: Fix gender tag (GH-61)

--- source/gallery.ts
+++ source/gallery.ts
@@ -27,9 +27,9 @@ export function getGallery(id: number): Promise<Gallery> {
 			for(let i: number = 0; i < responseJson['tags']['length']; i++) {
 				let type: Tag['type'] = 'tag';
 
-				if(responseJson['tags'][i]['male'] === 1) {
+				if(Number.parseInt(responseJson['tags'][i]['male']) === 1) {
 					type = 'male';
-				} else if(responseJson['tags'][i]['female'] === 1) {
+				} else if(Number.parseInt(responseJson['tags'][i]['female']) === 1) {
 					type = 'female';
 				}
 
