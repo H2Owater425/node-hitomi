56f6e3f6bd46c3a7bf9de24e983dfd6ec38b657a
H2Owater425 <h2o@dhmo.kr>
2026-05-17 15:41:21 +0900

    test: Avoid using TAG_TYPES directly

--- tests/utilities/constants.ts
+++ tests/utilities/constants.ts
@@ -1,7 +1,8 @@
 import { TextEncoder } from 'util';
+import type { Tag } from '../../source/tag';
 import { TAG_TYPES } from '../../source/utilities/constants';
 
-export const PARTIAL_TAG_TYPES: typeof TAG_TYPES = TAG_TYPES;
+export const PARTIAL_TAG_TYPES: Set<Tag['type']> = new Set(TAG_TYPES);
 
 PARTIAL_TAG_TYPES.delete('language');
 PARTIAL_TAG_TYPES.delete('type');
