4169ad36b93b96790c58afca80141352eeb59f4b
H2Owater425 <h2o@dhmo.kr>
2026-05-27 05:40:05 +0900

    feat: Export platform shared types

--- source/index.ts
+++ source/index.ts
@@ -7,7 +7,7 @@ export { HitomiError } from './structures/error';
 export { Title, GalleryReference, TranslatedGallery, Gallery } from './structures/gallery';
 export { Extension, ThumbnailSize, Image, Video } from './structures/media';
 export { Language, Tag } from './structures/tag';
-export { type RequestContext, ResponseType } from '@platform';
+export { type RequestContext, type RequestFunction, type HashFunction, type OnRequestFunction, ResponseType } from '@platform';
 
 export const hitomi: Hitomi = new Hitomi();
 export default hitomi;
